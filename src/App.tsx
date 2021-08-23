96d63b74390f92e330b0713a34d4575a331ea9b3
Arthur Yan <arthuryan08@gmail.com>
2021-08-22 21:34:58 -0300

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Home } from "./pages/Home";
 
+import { BrowserRouter } from 'react-router-dom'
+import { NewRoom } from "./pages/NewRoom";
+
 function App() {
     return (
-        <Home/>
+        <NewRoom/>
     )
 }
 
