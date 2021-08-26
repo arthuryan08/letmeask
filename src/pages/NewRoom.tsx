827facd9997dda0b4afb6ec185fa63c32af1a8ed
Arthur Yan <arthuryan08@gmail.com>
2021-08-26 19:00:21 -0300

    form submit

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from '../components/Button'
+import { database } from "../services/firebase";
 import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
 
 export function NewRoom() {
-    // const { user } = useAuth()
+    const { user } = useAuth()
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (newRoom.trim() === ''){
+            return;
+        }
+        
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId
+        })
+    }
 
     return (
         <div id="page-auth">
@@ -22,10 +40,12 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
